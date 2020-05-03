e207621a3fe1ea8b81c9c009f08279298e2c4d9a
Elkin Huertas <mauricioelkin@live.com>
2020-05-02 22:00:33 -0400

    created pixel to em calculator with js

--- javascript starter files/20.Time to practice-px to em calculator/script.js
+++ javascript starter files/20.Time to practice-px to em calculator/script.js
@@ -1,1 +1,16 @@
 // em = required / parent 
+
+var calculate = () => {
+    var parentVal = document.getElementById('parent').value;
+    var requiredVal = document.getElementById('required').value;
+    var result =  document.getElementById('result');
+
+    if(!parentVal | !requiredVal){
+        alert('Please fill in all fields');
+    }else{
+        result.innerHTML =  requiredVal / parentVal;
+    }
+}
+
+var btn = document.querySelector('button');
+btn.addEventListener('click', calculate);
