ae261c099c0a56b296d8fe38a413ea65907ad1fb
Elkin Huertas <mauricioelkin@live.com>
2020-05-04 20:51:44 -0400

    initialized const array

---
+++ javascript starter files/31.Shape matcher game/script.js
@@ -0,0 +1,19 @@
+let currentScore = 0;
+let playing = false;
+let shape1;
+let shape2;
+
+const shapes = [
+    {color: '#FF595E', width: 250, height: 160},
+    {color: '#FF595E', width: 270, height: 150},
+    {color: '#FFCA3A', width: 320, height: 170},
+    {color: '#FFCA3A', width: 310, height: 180},
+    {color: '#8AC926', width: 380, height: 160},
+    {color: '#8AC926', width: 300, height: 175},
+    {color: '#1982C4', width: 400, height: 185},
+    {color: '#1982C4', width: 250, height: 120},
+    {color: '#6A4C93', width: 440, height: 160},
+    {color: '#6A4C93', width: 250, height: 160},
+];
+
+console.log(shapes[2]);
